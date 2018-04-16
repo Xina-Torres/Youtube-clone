4a8e8ebc5dbfe17df315b60ed1643ab9cabd0cc0
Xina-Torres <xina.torres@gmail.com>
2018-04-16 14:56:58 +0800

    Initial Commit

---
+++ Youtube-Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube-Clone
+//
+//  Created by Maria Xina Rae Torres on 16/04/2018.
+//  Copyright © 2018 Maria Xina Rae Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
